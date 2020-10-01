2312caa963f79fb4302839ee9dea940dd1f99db1
max <vertex.color+github@protonmail.com>
2020-10-01 16:46:31 +0200

    Added missing material & namespace.

--- Samples~/Example1/Scripts/Rotator.cs
+++ Samples~/Example1/Scripts/Rotator.cs
@@ -1,15 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Rotator : MonoBehaviour
+namespace MA_Toolbox.MA_HeightMapCaptureRig
 {
-    [SerializeField]
-    private float m_speed = 10.0f;
-
-    // Update is called once per frame
-    void Update()
+    public class Rotator : MonoBehaviour
     {
-        this.transform.Rotate(Vector3.up * Time.deltaTime * m_speed, Space.Self);
+        [SerializeField]
+        private float m_speed = 10.0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            this.transform.Rotate(Vector3.up * Time.deltaTime * m_speed, Space.Self);
+        }
     }
 }
